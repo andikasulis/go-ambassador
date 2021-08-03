d48ed80f8328b2e7d6e8589354316791d1363cde
Andika Sulis <andikasulispratama@yahoo.co.id>
2021-08-03 19:17:47 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+
+	if err != nil{
+		panic("Could not connect with the dataabse!")
+	}
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":3000")
+}
