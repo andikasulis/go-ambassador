1991d208ab2796167a85b3cc9e12e1e14a8d661c
Andika Sulis <andikasulispratama@yahoo.co.id>
2021-08-05 23:16:29 +0700

    Implement security header response

--- src/middlewares/auth.go
+++ src/middlewares/auth.go
@@ -9,6 +9,14 @@ import (
 func IsAuthenticated(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
 
+	// Set some security headers:
+	c.Set("X-XSS-Protection", "1; mode=block")
+	c.Set("X-Content-Type-Options", "nosniff")
+	c.Set("X-Download-Options", "noopen")
+	c.Set("Strict-Transport-Security", "max-age=5184000")
+	c.Set("X-Frame-Options", "SAMEORIGIN")
+	c.Set("X-DNS-Prefetch-Control", "off")
+
 	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte("secret"), nil
 	})
